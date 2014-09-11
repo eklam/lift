2d5fb28dc668ee8fdce2cbe4fe66ac7e4af74f2d
rmalke <malke.eklam@gmail.com>
2014-09-10 21:53:30 -0300

    Adding summary to ILinfIndexTemplate

--- src/ILinfIndexTemplate.cs
+++ src/ILinfIndexTemplate.cs
@@ -3,6 +3,9 @@ using System.Linq;
 
 namespace Eklam.Lift
 {
+    /// <summary> 
+    /// Implement this interface in a partial class representing your CRUD Index Template
+    /// </summary>
     public interface ILinfIndexTemplate
     {
         Type Data { get; set; }
